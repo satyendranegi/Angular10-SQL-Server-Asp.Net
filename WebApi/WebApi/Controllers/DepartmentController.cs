416f362da3a02210322f10b22f3a666e6f98aeea
Satyendra_Negi <satyendranegi3@gmail.com>
2021-02-25 10:31:54 +0530

    angular 10 with SQL Sever and Asp.Net WebApi

---
+++ WebApi/WebApi/Controllers/DepartmentController.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+using WebApi.Models;
+
+namespace WebApi.Controllers
+{
+    public class DepartmentController : ApiController
+    {
+        public HttpResponseMessage Get()
+        {
+            string query = @"select DepartmentId, DepartmentName from dbo.Department";
+            DataTable table = new DataTable();
+            using(var con = new SqlConnection(ConfigurationManager.ConnectionStrings["EmployeeAppDB"].ConnectionString))
+            using (var cmd = new SqlCommand(query,con))
+                using(var da = new SqlDataAdapter(cmd))
+            {
+                cmd.CommandType = CommandType.Text;
+                da.Fill(table);
+            }
+            return Request.CreateResponse(HttpStatusCode.OK, table);
+        }
+        public string Post(Department dep)
+        {
+            try
+            {
+                string query = @"insert into dbo.Department values ('" + dep.DepartmentName + @"')";
+                DataTable table = new DataTable();
+                using (var con = new SqlConnection(ConfigurationManager.ConnectionStrings["EmployeeAppDB"].ConnectionString))
+                using (var cmd = new SqlCommand(query, con))
+                using (var da = new SqlDataAdapter(cmd))
+                {
+                    cmd.CommandType = CommandType.Text;
+                    da.Fill(table);
+                }
+                return "Added Successfully!!";
+            }
+            catch (Exception)
+            {
+
+                return "Failed to add!!";
+            }
+        }
+        public string Put(Department dep)
+        {
+            try
+            {
+                string query = @"update dbo.Department set DepartmentName='" + dep.DepartmentName + @"' where DepartmentId="+dep.DepartmentId+@"";
+                DataTable table = new DataTable();
+                using (var con = new SqlConnection(ConfigurationManager.ConnectionStrings["EmployeeAppDB"].ConnectionString))
+                using (var cmd = new SqlCommand(query, con))
+                using (var da = new SqlDataAdapter(cmd))
+                {
+                    cmd.CommandType = CommandType.Text;
+                    da.Fill(table);
+                }
+                return "Added Successfully!!";
+            }
+            catch (Exception)
+            {
+
+                return "Failed to add!!";
+            }
+        }
+        public string Delete(int id)
+        {
+            try
+            {
+                string query = @"Delete from dbo.Department where DepartmentId=" + id + @"";
+                DataTable table = new DataTable();
+                using (var con = new SqlConnection(ConfigurationManager.ConnectionStrings["EmployeeAppDB"].ConnectionString))
+                using (var cmd = new SqlCommand(query, con))
+                using (var da = new SqlDataAdapter(cmd))
+                {
+                    cmd.CommandType = CommandType.Text;
+                    da.Fill(table);
+                }
+                return "Deleted Successfully!!";
+            }
+            catch (Exception)
+            {
+
+                return "Failed to Delete!!";
+            }
+        }
+        [Route("api/Department/SaveFile")]
+        public string SaveFile()
+        {
+            try
+            {
+                var httpRequest = HttpContext.Current.Request;
+                var postedFile = httpRequest.Files[0];
+                string filename = postedFile.FileName;
+                var physicalPath = HttpContext.Current.Server.MapPath("~/Photos/"+filename);
+                postedFile.SaveAs(physicalPath);
+                return filename;
+            }
+            catch (Exception)
+            {
+
+                return "anonymous.png";
+            }
+        }
+    }
+}
---
+++ WebApi/WebApi/ReadMe.txt
@@ -0,0 +1,25 @@
+﻿Datatbase
+
+--create table dbo.Employee(
+--EmployeeId int identity(1,1),
+--EmployeeName varchar(500),
+--Department varchar(500),
+--DateOfJoining date,
+--PhotoFileName varchar(500)
+--)
+
+--insert into dbo.Employee values('satyen','IT','2020-06-01','ssn.png')
+
+select * from dbo.Employee
+
+------------------------------------------------------------------------
+--create table dbo.Department(
+--DepartmentId int identity(1,1),
+--DepartmentName varchar(500)
+--)
+
+select * from Department
+
+--insert into Department values('Support')
+-------------------------------------------------------------------------------------------------------------
+Install-Package Microsoft.AspNet.WebApi.Cors
